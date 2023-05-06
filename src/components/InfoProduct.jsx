8c71a5d9f9b4204ee44eddba5556d469e4109e4b
cordovabarbara <aliendocordova@gmail.com>
2023-05-06 08:31:57 -0500

    reset styles index and add font

--- src/components/InfoProduct.jsx
+++ src/components/InfoProduct.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import './styles/infoproduct.css'
+import React from "react";
+import "./styles/infoproduct.css";
 
 const InfoProduct = ({ product }) => {
-    console.log( product )
+    console.log(product);
 
-  return (
-    <article>
-        <header>
-            <img src={product.images[0].url} alt=''/>
-        </header>
-        <section>
-            <h4>{product.brand}</h4>
-            <h3>{product.title}</h3>
-        </section>
+    return (
+        <article>
+            <header>
+                <img src={product.images[0].url} alt="" />
+            </header>
+            <section>
+                <h4>{product.brand}</h4>
+                <h3>{product.title}</h3>
+            </section>
             <div>
                 <span>Price</span>
                 <span>{product.price}</span>
             </div>
             <button>
-            <i class="bi bi-cart3"></i>
+                <i class="bi bi-cart3"></i>
             </button>
-    </article>
-  )
-}
+        </article>
+    );
+};
 
-export default InfoProduct
+export default InfoProduct;
