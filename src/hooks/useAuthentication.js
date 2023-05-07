03461e26b35f6dbea6a8d833bfdaabe0197193ae
cordovabarbara <aliendocordova@gmail.com>
2023-05-07 11:54:27 -0500

    create: login, form to login localStorage function

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -8,7 +8,22 @@ const useAuthentication = () => {
         .then(res => console.log(res.data))
         .catch (error => console.log(error))
     }
-    return { createNewUser }
+
+    const loginUser = data => {
+        const url = `https://e-commerce-api-v2.academlo.tech/api/v1/users/login`
+        axios.post(url, data)
+        .then(res => {
+            localStorage.setItem('token', res.data.token)
+        console.log(res.data)
+        })
+        .catch (error => {
+            console.log(error)
+            localStorage.removeItem('token')
+        })
+    }
+
+
+    return { createNewUser, loginUser }
 }
 
 export default useAuthentication
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import useAuthentication from '../hooks/useAuthentication'
 
 const Login = () => {
+
+    const { loginUser }= useAuthentication ()
+
+    const handleLogin = e => {
+        e.preventDefault()
+        const email = e.target.email.value
+        const password = e.target.password.value
+        const data = { email, password }
+        loginUser(data)
+    }
+
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleLogin}>
+        <h2>Login</h2>
+        <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email'></input>
+        </div>
+
+    <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" id='password'></input>
+        </div>
+    <button> Log in</button>
+    </form>
   )
 }
 
