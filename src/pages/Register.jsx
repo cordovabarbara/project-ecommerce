65eff0944ad6c6738c4f6f35879a7bee27de9ad5
cordovabarbara <aliendocordova@gmail.com>
2023-05-07 11:10:55 -0500

    create register and form, add reset and defaultValues

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import useAuthentication from '../hooks/useAuthentication'
+import defaultValues from '../utils/defaultValues'
 
 const Register = () => {
+
+
+const { register, handleSubmit, reset } = useForm ()
+
+const { createNewUser } = useAuthentication ()
+
+
+const submit = data => {
+    createNewUser(data)
+    reset(defaultValues)
+
+}
+
+
   return (
-    <div>Register</div>
+    <form onSubmit={handleSubmit (submit)}>
+    <h2> Create a new User</h2>
+    <div>
+    <label htmlFor="firstName">First Name</label>
+    <input {...register('firstName')}type="text" id='firstName'></input>
+    </div>
+
+    <div>
+    <label htmlFor="lastName">Last Name</label>
+    <input {...register('lastName')} type="text" id='lastName'></input>
+    </div>
+
+    <div>
+    <label htmlFor="email">Email</label>
+    <input {...register('email')} type="email" id='email'></input>
+    </div>
+
+    <div>
+    <label htmlFor="password">Password</label>
+    <input {...register('password')} type="password" id='password'></input>
+    </div>
+
+    <div>
+    <label htmlFor="phone">Phone</label>
+    <input {...register('phone')} type="tel" id='phone'></input>
+    </div>
+    <button>Register</button>
+    </form>
   )
 }
 
