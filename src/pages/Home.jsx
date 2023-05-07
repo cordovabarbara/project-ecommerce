32e360a39f69bc514588e12a5e78246aa5e8f196
cordovabarbara <aliendocordova@gmail.com>
2023-05-07 12:06:01 -0500

    create: css to homeproduct, adjust row

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import InfoProduct from '../components/InfoProduct';
+import './styles/homeProduct.css'
 
 const Home = () => {
 
 const { productsGlobal }  = useSelector (state => state)
 
   return (
-    <div>
+    <div className='home__product'>
       {
         productsGlobal?.map(prod => (
           <InfoProduct
