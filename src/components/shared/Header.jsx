cfe3dddc8ff2cdc0823ad1f7d6272183c1475a18
cordovabarbara <aliendocordova@gmail.com>
2023-05-09 18:45:49 -0500

    add/ create classname to work in css

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-      <header>
-          <h1><Link to='/'>E-commerce</Link></h1>
-          <nav>
+      <header className='header__box'>
+          <h1 className='header__title'><Link to='/'>E-commerce</Link></h1>
+          <nav className='navbar'>
               <ul>
                   <li><Link to='/login'>Login</Link></li>
                   <li><Link to='/register'>Register</Link></li>
