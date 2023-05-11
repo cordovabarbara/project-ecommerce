a513a28b7b65fb0d29b1b93737f36c895e4118b5
cordovabarbara <aliendocordova@gmail.com>
2023-05-11 01:41:55 -0500

    add / new functionality to slider to change de images

--- src/components/AllProducts/SliderImg.jsx
+++ src/components/AllProducts/SliderImg.jsx
@@ -1,18 +1,47 @@
+import { useState } from 'react'
 import '../styles/sliderimg.css'
 
 const SliderImg = ({product}) => {
+
+  const [changeImg, setChangeImg] = useState(0)
+
+
+  const objStyle = {
+    transform: `translateX(calc(-${changeImg} /3 * 100%))`
+  }
+
+  const handlePrevious = () =>{
+   if (changeImg - 1 < 0 ){
+    setChangeImg(2)
+  }else {
+    setChangeImg(changeImg -1)
+   }
+  }
+
+  const handleNext = () =>{
+    if(changeImg +1 >2){
+      setChangeImg(0)
+    }else{
+      setChangeImg(changeImg + 1)
+    }
+
+  }
+
+
   return (
     <div className='slider__prod'>
-    <button  className='slider__btn'>&lt;</button>
-      <div className='slider__mov'>
+    <button onClick={handlePrevious}  className='slider__btn slider__left'>&lt;</button>
+      <div style={objStyle} className='slider__mov'>
         {
         product?.images.map(imgProd =>(
-          <div className='slider__box-img'>
-          <img className='slider__img' key={imgProd.id} src={imgProd.url} alt=''/></div>
+          <div key={imgProd.id} className='slider__box-img'>
+          <img 
+          className='slider__img' 
+          src={imgProd.url} alt=''/></div>
         ))
         }
       </div>
-    <button className='slider__btn slider__right' >&gt;</button>
+    <button onClick={handleNext}  className='slider__btn slider__right' >&gt;</button>
   </div>
   )
 }
