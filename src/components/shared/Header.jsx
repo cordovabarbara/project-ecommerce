195dc1e1bad1fdec658826f588592a1c90c60ded
cordovabarbara <aliendocordova@gmail.com>
2023-11-09 20:25:33 -0500

    change navbar

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -2,25 +2,37 @@
 import { Link } from 'react-router-dom'
 import '../styles/header.css';
 import mundo from '/public/mundopc.png'
+import { useState } from 'react';
 
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
     return (
       <header className='header__box'>
           <div className='header__logo'>
             <Link to='/'><img className='img__logo' src={mundo} alt="Home"/></Link></div>
             <nav className='navbar'>
-              <ul className='navbar__ul'>
-                <il>
-                  Registrarse
-                </il>
-                <il>
-                  Login
-                </il>
-                <il>
-                  Carrito
-                </il>
+            <li onClick={handleMenu}>
+                <i className='bx bx-menu'></i>
+                </li>
+              <ul className={`navbar__ul ${menuOpen ? 'open' : ''}`}>
+                <li title= "Registrarse">
+                <i className='bx bxs-user-plus'></i>
+                </li>
+                <li title= "Ingresar">
+                <i className='bx bxs-user-circle'></i>
+                </li>
+                <li title= "Compras">
+                <i className='bx bxs-shopping-bags'></i>
+                </li>
+                <li title="Home">
+                <i className='bx bxs-home'></i>
+                </li>
               </ul>
             </nav>
             
