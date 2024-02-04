aa951a00f413199e6cb2a25782209c54b76f25f1
cordovabarbara <aliendocordova@gmail.com>
2024-02-04 16:46:16 -0500

    change register zone

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useForm } from 'react-hook-form'
 import useAuthentication from '../hooks/useAuthentication'
 import defaultValues from '../utils/defaultValues'
+import '../pages/styles/register.css'
 
 const Register = () => {
 
@@ -18,34 +19,41 @@ const submit = data => {
 
 
   return (
-    <form onSubmit={handleSubmit (submit)}>
-    <h2>Si aun no tienes cuentas, Registrate</h2>
-    <div>
-    <label htmlFor="firstName">Primer Nombre</label>
+    <div className='login__ppl'>
+    <form className='register__form' onSubmit={handleSubmit (submit)}>
+    <div className='register__box'>
+    
+    <h2>Si aun no tienes cuenta, Registrate!</h2>
+    <div className='register__ttl'>
+    <label className='label__register' htmlFor="firstName">Primer Nombre</label>
     <input {...register('firstName')}type="text" id='firstName'></input>
     </div>
 
-    <div>
+    <div  className='register__ttl'>
     <label htmlFor="lastName">Apellido</label>
     <input {...register('lastName')} type="text" id='lastName'></input>
     </div>
 
-    <div>
+    <div  className='register__ttl'>
     <label htmlFor="email">Email</label>
     <input {...register('email')} type="email" id='email'></input>
     </div>
 
-    <div>
+    <div  className='register__ttl'>
     <label htmlFor="password">Password</label>
     <input {...register('password')} type="password" id='password'></input>
     </div>
 
-    <div>
+    <div  className='register__ttl'>
     <label htmlFor="phone">Telefono</label>
     <input {...register('phone')} type="tel" id='phone'></input>
     </div>
-    <button>Registrarse</button>
+    <button className='log__btn regiter__btn'>Registrarse</button>
+
+    </div>
     </form>
+    </div>
+
   )
 }
 
